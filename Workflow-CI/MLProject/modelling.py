a3d813e73f26c9e02094055aeeebdd85eb9f0c23
pitriabais <baispitria@gmail.com>
2026-06-01 14:50:29 +0700

    perbaikan: menghapus start run manual dan mengaktifkan autolog

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -10,30 +10,21 @@ os.environ['MLFLOW_TRACKING_USERNAME'] = 'pitriabais'
 os.environ['MLFLOW_TRACKING_PASSWORD'] = '70059272253e0f09e6d4e535f41a5ebf493db8ff'
 mlflow.set_tracking_uri('https://dagshub.com/pitriabais/Eksperimen_SML_Pitria-Bais-Mawarti.mlflow')
 
-mlflow.set_experiment("Heart_Disease_Modelling_Pitria")
+# Aktifkan auto-logging agar semua parameter & model tercatat otomatis tanpa start_run manual
+mlflow.sklearn.autolog()
 
 def main():
     print("[INFO] Membaca dataset preprocessing lokal...")
     train_df = pd.read_csv("heartdisease_preprocessing/train_clean.csv")
-    test_df = pd.read_csv("heartdisease_preprocessing/test_clean.csv")
     
     X_train = train_df.iloc[:, :-1]
     y_train = train_df.iloc[:, -1]
     
-    # Memulai pencatatan otomatis alur CI
-    with mlflow.start_run(run_name="CI_Automated_Retrain", nested=True):
-        print("[INFO] Melatih ulang model via GitHub Actions...")
-        
-        model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
-        model.fit(X_train, y_train)
-        
-        # Simpan parameter dasar
-        mlflow.log_param("model_type", "RandomForest_CI")
-        mlflow.log_param("status", "Retrained_Automatically")
-        
-        # Log model ke DagsHub agar dapet run_id untuk Docker Build
-        mlflow.sklearn.log_model(model, "model")
-        print("[SUCCESS] Model baru berhasil dikirim ke DagsHub!")
+    print("[INFO] Melatih ulang model via GitHub Actions...")
+    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
+    model.fit(X_train, y_train)
+    
+    print("[SUCCESS] Model baru berhasil dilatih dan dicatat otomatis ke DagsHub!")
 
 if __name__ == "__main__":
     main()
