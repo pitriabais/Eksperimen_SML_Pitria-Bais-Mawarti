5ec570407f251ccdff0d1e65cc60d7c25e4cca4b
pitriabais <baispitria@gmail.com>
2026-06-01 15:16:22 +0700

    perbaikan: menggunakan active_run untuk menghindari konflik id run dagsHub

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -5,10 +5,12 @@ import mlflow
 import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 
+# Konfigurasi Kredensial Otomatis ke DagsHub Pitria
 os.environ['MLFLOW_TRACKING_USERNAME'] = 'pitriabais'
 os.environ['MLFLOW_TRACKING_PASSWORD'] = '70059272253e0f09e6d4e535f41a5ebf493db8ff'
 mlflow.set_tracking_uri('https://dagshub.com/pitriabais/Eksperimen_SML_Pitria-Bais-Mawarti.mlflow')
 
+# Aktifkan autolog sebelum melatih model
 mlflow.sklearn.autolog()
 
 def main():
@@ -19,16 +21,19 @@ def main():
     y_train = train_df.iloc[:, -1]
     
     print("[INFO] Melatih ulang model via GitHub Actions...")
+    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
+    model.fit(X_train, y_train)
     
-    with mlflow.start_run() as run:
-        model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
-        model.fit(X_train, y_train)
+    active_run = mlflow.active_run()
+    if active_run:
+        run_id = active_run.info.run_id
+    else:
+        run_id = "default_run"
         
-        run_id = run.info.run_id
-        print(f"[SUCCESS] Model berhasil dilatih dengan Run ID: {run_id}")
-        
-        with open("run_id.txt", "w") as f:
-            f.write(run_id)
+    print(f"[SUCCESS] Model berhasil dilatih dengan Run ID: {run_id}")
+    
+    with open("run_id.txt", "w") as f:
+        f.write(run_id)
 
 if __name__ == "__main__":
     main()
