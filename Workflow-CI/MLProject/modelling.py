3504508e81099ada3739c442fe5c14dccb258d47
pitriabais <baispitria@gmail.com>
2026-06-01 15:10:14 +0700

    perbaikan: menggunakan file txt untuk oper data run id secara akurat

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -5,12 +5,10 @@ import mlflow
 import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 
-# Konfigurasi Kredensial Otomatis ke DagsHub Pitria
 os.environ['MLFLOW_TRACKING_USERNAME'] = 'pitriabais'
 os.environ['MLFLOW_TRACKING_PASSWORD'] = '70059272253e0f09e6d4e535f41a5ebf493db8ff'
 mlflow.set_tracking_uri('https://dagshub.com/pitriabais/Eksperimen_SML_Pitria-Bais-Mawarti.mlflow')
 
-# Aktifkan auto-logging agar semua parameter & model tercatat otomatis tanpa start_run manual
 mlflow.sklearn.autolog()
 
 def main():
@@ -21,10 +19,16 @@ def main():
     y_train = train_df.iloc[:, -1]
     
     print("[INFO] Melatih ulang model via GitHub Actions...")
-    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
-    model.fit(X_train, y_train)
     
-    print("[SUCCESS] Model baru berhasil dilatih dan dicatat otomatis ke DagsHub!")
+    with mlflow.start_run() as run:
+        model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
+        model.fit(X_train, y_train)
+        
+        run_id = run.info.run_id
+        print(f"[SUCCESS] Model berhasil dilatih dengan Run ID: {run_id}")
+        
+        with open("run_id.txt", "w") as f:
+            f.write(run_id)
 
 if __name__ == "__main__":
     main()
